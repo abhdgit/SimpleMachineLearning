c8dbf97a1ae8be993961e247bd633180c3c1ede0
李德华 <dehua.li@fengjr.com>
2019-03-18 12:26:34 +0800

    增加牛顿法

--- optimization/search.py
+++ optimization/search.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
-
+import sympy
 
 class singleSearch:
     @staticmethod
     def golden_selection(func,l,r,interval):
+        """
+        区间缩小法中的黄金分割点发进行最小值点逼近
+        
+        Parameters
+        ----------
+        func:要求解的函数式
+        l:区间左侧值
+        r:区间右侧值
+        interval:迭代停止准则
+        
+        Returns
+        --------
+        返回最小值点处的x值
+        """
         #区间小于interval迭代停止
         if r-l<interval:
             return (l+r)/2
@@ -22,8 +36,45 @@ class singleSearch:
             return singleSearch.golden_selection(func,l,rr,interval)
         else :
             return singleSearch.golden_selection(func,ll,rr,interval)
+    @staticmethod    
+    def newton_method(func,start,interval):
+        """
+        牛顿法计算函数最小值点
+        Parameters
+        -------------
+        func:求解最小值点的函数
+        start:初始迭代点
+        interval:停止准则
+        
+        Returns
+        ----------
+        返回最小值点处的x值
+        """
+        
+        def helper(oneD,twoD,start,interval):
+            t = sympy.Symbol('x')
+            #计算一维导数和二维导数值
+            oneD_value=oneD.subs(t,start)
+            twoD_value=twoD.subs(t,start)
+            #新的迭代点值
+            iter_value=start-oneD_value/twoD_value
+            #判断是否满足停止准则
+            if abs(iter_value-start)<interval:
+                return iter_value
+            else:
+                return helper(oneD,twoD,iter_value,interval)
+        #计算函数的一阶和二阶导数
+        x = sympy.Symbol('x')
+        oneD=sympy.diff(func,x,1)
+        twoD=sympy.diff(func,x,2)
+        #helper函数求解新的迭代点
+        out=helper(oneD,twoD,start,interval)
+        return out
             
 if __name__=="__main__":
     def cal(x):
-        return 2*x**2+20*x
+        return 2*x**2+13*x
     a=singleSearch.golden_selection(cal,-100,50,0.01)
+    x = sympy.Symbol('x')
+    f=2*x**2+13*x
+    b=float(singleSearch.newton_method(f,100,0.0001))
